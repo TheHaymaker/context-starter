b0fb01ee5b782b65427f6f7dedae7a00f70a413b
Brett Haymaker <brett.haymaker@gmail.com>
2019-04-10 17:42:15 -0500

    adds notes

--- src/App.js
+++ src/App.js
@@ -25,10 +25,17 @@ const NestedElement = ({ children }) => {
   return <div>{children}</div>;
 };
 
+//  This will re-render every single time the Context value is changed
 const ContextConsumingElement = () => {
+  // equivalent to static contextType = MyContext in a class
+  //  or to <MyContext.Consumer>.
+  // useContext(MyContext) only lets you read the context
+  // and subscribe to its changes. You still need a <MyContext.Provider>
+  // above in the tree to provide the value for this context.
   const theme = useContext(ThemeContext);
   const styles = theme === 'light' ? { backgroundColor: 'white' } : { backgroundColor: 'grey' };
   console.log(theme, styles);
+
   return <button style={styles}>Themeable button</button>;
 };
 
